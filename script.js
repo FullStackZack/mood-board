c079b785da31ae0e39ce297b1b95a2bde0a41577
Zack White <zhwhite18@yahoo.com>
2020-08-19 13:40:39 -0500

    api

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 $("#goBtn").on("click", function(event) {
     event.preventDefault();
 
+    window.location.href = "moodi.html";
+    return false;
+
+});
+
+function changeColor() {
     
-})
+}
+
+var movieQueryURL = "https://api.themoviedb.org/3/trending/movie/day?api_key=ec99a176cace44f78bc5aa86b27b4186";
+
+
+    $.ajax({
+        url: movieQueryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response);
+
+        var movieTitle = response.results[15].title;
+
+        var p1 = $("<p>").text(movieTitle);
+
+        $("#suggestion-bubbles").append(p1)
+
+    })
